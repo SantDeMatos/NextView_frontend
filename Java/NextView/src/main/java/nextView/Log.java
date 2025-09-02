c814cbb753f58b697a58040178b46c9fdf968f48
guilherme-dias018 <guilherme.rdias@sptech.school>
2025-09-02 20:37:40 -0300

    Log do projeto v1

--- Java/NextView/src/main/java/nextView/Log.java
+++ Java/NextView/src/main/java/nextView/Log.java
@@ -1,7 +1,21 @@
 package nextView;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Log {
 
+    public static void main(String[] args) {
 
+        LocalDateTime data = LocalDateTime.now();
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        String dataFormatada = formato.format(data);
+
+        System.out.println(dataFormatada);
+
+        // Estrutura de logs: Guilherme Enrique Dias
+
+    }
 
 }
