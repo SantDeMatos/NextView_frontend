72de76c07c879290b53a6826e15ebf27104116ba
SantDeMatos <matheus.smatos@sptech.school>
2025-09-15 23:14:18 -0300

    modificações no script de banco de dados

--- website/src/database/database.sql
+++ website/src/database/database.sql
@@ -2,13 +2,13 @@ CREATE DATABASE nextview;
 
 USE nextview;
 
+
 CREATE TABLE empresa (
   idEmpresa INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   nomeEmpresa VARCHAR(50) NOT NULL,
-  nome VARCHAR(50) NOT NULL,
-  emailEmpresa VARCHAR(50) NOT NULL,
-  cnpjEmpresa VARCHAR(14) UNIQUE NOT NULL,
-  telefoneEmpresa VARCHAR(11) NOT NULL,
+   cnpjEmpresa VARCHAR(18) UNIQUE NOT NULL,
+  emailEmpresa VARCHAR(50) NOT NULL, 
+  telefoneEmpresa VARCHAR(14) NOT NULL,
   senhaEmpresa VARCHAR(50) NOT NULL
 );
 
@@ -17,8 +17,10 @@ CREATE TABLE contato (
   nomeContato VARCHAR(45) NOT NULL,
   emailContato VARCHAR(50) NOT NULL,
   mensagemContato VARCHAR(200) NOT NULL,
-  telefoneContato VARCHAR(11) NOT NULL
+  telefoneContato VARCHAR(14) NOT NULL
 );
+
+
 
 select * from contato;
 
