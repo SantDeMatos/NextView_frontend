8fa7e551865314be9cd84434933e03db116d3213
mateus <mateus.jimenez@sptech.school>
2025-09-25 11:55:41 -0300

    realização da criptografia de senha

--- website/src/database/database.sql
+++ website/src/database/database.sql
@@ -1,26 +1,21 @@
 CREATE DATABASE nextview;
-
 USE nextview;
 
-
 CREATE TABLE empresa (
-  idEmpresa INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
   nomeEmpresa VARCHAR(50) NOT NULL,
-   cnpjEmpresa VARCHAR(18) UNIQUE NOT NULL,
-  emailEmpresa VARCHAR(50) NOT NULL, 
-  telefoneEmpresa VARCHAR(14) NOT NULL,
-  senhaEmpresa VARCHAR(50) NOT NULL
+  emailEmpresa VARCHAR(50) NOT NULL,
+  cnpjEmpresa VARCHAR(14) UNIQUE NOT NULL,
+  telefoneEmpresa VARCHAR(11) NOT NULL,
+  senhaEmpresa VARCHAR(128) NOT NULL
 );
-
 CREATE TABLE contato (
-  idContato INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  nomeContato VARCHAR(45) NOT NULL,
-  emailContato VARCHAR(50) NOT NULL,
-  mensagemContato VARCHAR(200) NOT NULL,
-  telefoneContato VARCHAR(14) NOT NULL
+    idContato INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nomeContato VARCHAR(45) NOT NULL,
+    emailContato VARCHAR(50) NOT NULL,
+    mensagemContato VARCHAR(200) NOT NULL,
+    telefoneContato VARCHAR(14) NOT NULL
 );
-
-
 
 select * from contato;
 
