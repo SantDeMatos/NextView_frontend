2ef7c1075acc26b3ca77569b2aedaf48dc879f2d
SantDeMatos <matheus.smatos@sptech.school>
2025-10-01 07:46:49 -0300

    adição da tabela log no banco de dados

--- website/src/database/database.sql
+++ website/src/database/database.sql
@@ -17,6 +17,17 @@ CREATE TABLE contato (
     telefoneContato VARCHAR(14) NOT NULL
 );
 
+CREATE TABLE log(
+
+idLog INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+dtLog DATETIME NOT NULL,
+descLog VARCHAR(255) NOT NULL,
+qtdErro INT NOT NULL
+);
+
+
+
+
 select * from contato;
 
 select * from empresa;
